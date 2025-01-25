0cd2bd2f1d55225a134918833c8c21bcfe25836b
Gabriel Reis Franzen <152448019+gabrielfranzen@users.noreply.github.com>
2025-01-25 11:43:01 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/flask'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/flask_task_manager'
